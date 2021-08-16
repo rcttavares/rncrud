ff4b4b788bdfde0f415271332ab57b1fa0ec1b42
Rafael Tavares <rafael.tawares@gmail.com>
2021-08-16 17:49:24 -0300

    Add stack navigation

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import UserForm from './views/UserForm';
+import UserList from './views/UserList';
+import {Button, Icon} from 'react-native-elements';
+
+const Stack = createNativeStackNavigator();
+
+export default props => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="UserList"
+        screenOptions={screenOptions}>
+        <Stack.Screen
+          name="UserForm"
+          component={UserForm}
+          options={{title: 'Formulário de Usuários'}}
+        />
+        <Stack.Screen
+          name="UserList"
+          component={UserList}
+          options={({navigation}) => {
+            return {
+              title: 'Lista de Usuários',
+              headerRight: () => (
+                <Button
+                  type="clear"
+                  icon={<Icon name="add" size={25} color="white" />}
+                  onPress={() => navigation.navigate('UserForm')}
+                />
+              ),
+            };
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: '#f4511e',
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+};
