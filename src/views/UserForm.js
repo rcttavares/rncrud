f48f6afb5925138a1b126d3d156eda959956f020
Rafael Tavares <rafael.tawares@gmail.com>
2021-08-18 11:48:24 -0300

    Add user form component

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,6 +1,54 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
-export default props => {
-  return <Text>UserForm</Text>;
+export default ({route, navigation}) => {
+  const [user, setUser] = useState(route.params ? route.params : {});
+
+  return (
+    <View style={style.form}>
+      <Text>Nome</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={name => setUser({...user, name})}
+        placeholder="Informe o nome"
+        value={user.name}
+      />
+
+      <Text>E-mail</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={email => setUser({...user, email})}
+        placeholder="Informe o e-mail"
+        value={user.email}
+      />
+
+      <Text>URL do Avatar</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={avatarUrl => setUser({...user, avatarUrl})}
+        placeholder="Informe a URL do Avatar"
+        value={user.avatarUrl}
+      />
+
+      <Button
+        title="Salvar"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
+    </View>
+  );
 };
+
+const style = StyleSheet.create({
+  form: {
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 15,
+  },
+});
